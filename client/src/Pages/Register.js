e9dec70b3cb6bdf83799f986ba3a8be04273e3ae
Dani Domínguez <dani.d.zamorano@gmail.com>
2021-01-30 16:34:40 +0100

    call to register mutation and add error messages

--- client/src/Pages/Register.js
+++ client/src/Pages/Register.js
@@ -1,21 +1,43 @@
 import { useState } from 'react';
+import { gql, useMutation } from '@apollo/client';
+
+const REGISTER_USER = gql`
+  mutation register($username: String!, $email: String!, $password: String!, $confirmPassword: String!) {
+    register(username: $username, email: $email, password: $password, confirmPassword: $confirmPassword) {
+      username
+      email
+    }
+  }
+`;
 
 function Register() {
-  const [state, setState] = useState({
+  const [variables, setVariables] = useState({
     username: '',
     email: '',
     password: '',
     confirmPassword: '',
   });
 
+  const [errors, setErrors] = useState({});
+
+  const [registerUser, { loading }] = useMutation(REGISTER_USER, {
+    update(_, res) {
+      console.log(res);
+    },
+    onError(err) {
+      setErrors(err.graphQLErrors[0].extensions.errors);
+    },
+  });
+
   const submitRegisterForm = (e) => {
     e.preventDefault();
-    console.log(state);
+    setErrors({});
+    registerUser({ variables });
   };
 
   const handleInputChange = (e) => {
     const { name, value } = e.target;
-    setState({ ...state, [name]: value });
+    setVariables({ ...variables, [name]: value });
   };
 
   return (
@@ -30,11 +52,17 @@ function Register() {
           type="text"
           name="username"
           placeholder="John"
-          value={state.username}
-          className="block w-full p-3 mt-2 text-gray-700 bg-gray-200 appearance-none rounded focus:outline-none focus:bg-gray-300"
-          required
+          value={variables.username}
+          className={`block w-full p-3 mt-2 text-gray-700 bg-gray-100 appearance-none rounded focus:outline-none focus:bg-gray-200 ${
+            errors.username && 'border border-red-500'
+          }`}
           onChange={handleInputChange}
         />
+        {errors.username && (
+          <span class="flex items-center font-medium tracking-wide text-red-500 text-xs mt-1 ml-1">
+            {errors.username}
+          </span>
+        )}
         <label htmlFor="email" className="block mt-2 font-semibold text-gray-600">
           E-mail
         </label>
@@ -43,11 +71,13 @@ function Register() {
           type="email"
           name="email"
           placeholder="john.doe@company.com"
-          value={state.email}
-          className="block w-full p-3 mt-2 text-gray-700 bg-gray-200 appearance-none rounded focus:outline-none focus:bg-gray-300"
-          required
+          value={variables.email}
+          className={`block w-full p-3 mt-2 text-gray-700 bg-gray-100 appearance-none rounded focus:outline-none focus:bg-gray-200 ${
+            errors.email && 'border border-red-500'
+          }`}
           onChange={handleInputChange}
         />
+        <span class="flex items-center font-medium tracking-wide text-red-500 text-xs mt-1 ml-1">{errors.email}</span>
         <label htmlFor="password" className="block mt-2 font-semibold text-gray-600">
           Password
         </label>
@@ -56,11 +86,15 @@ function Register() {
           type="password"
           name="password"
           placeholder="********"
-          value={state.password}
-          className="block w-full p-3 mt-2 text-gray-700 bg-gray-200 appearance-none rounded focus:outline-none focus:bg-gray-300"
-          required
+          value={variables.password}
+          className={`block w-full p-3 mt-2 text-gray-700 bg-gray-100 appearance-none rounded focus:outline-none focus:bg-gray-200 ${
+            errors.password && 'border border-red-500'
+          }`}
           onChange={handleInputChange}
         />
+        <span class="flex items-center font-medium tracking-wide text-red-500 text-xs mt-1 ml-1">
+          {errors.password}
+        </span>
         <label htmlFor="password-confirm" className="block mt-2 font-semibold text-gray-600">
           Confirm password
         </label>
@@ -69,16 +103,21 @@ function Register() {
           type="password"
           name="confirmPassword"
           placeholder="********"
-          value={state.confirmPassword}
-          className="block w-full p-3 mt-2 text-gray-700 bg-gray-200 appearance-none focus:outline-none focus:bg-gray-300 focus:shadow-inner"
-          required
+          value={variables.confirmPassword}
+          className={`block w-full p-3 mt-2 text-gray-700 bg-gray-100 appearance-none rounded focus:outline-none focus:bg-gray-200 ${
+            errors.confirmPassword && 'border border-red-500'
+          }`}
           onChange={handleInputChange}
         />
+        <span class="flex items-center font-medium tracking-wide text-red-500 text-xs mt-1 ml-1">
+          {errors.confirmPassword}
+        </span>
         <button
           type="submit"
           className="w-full py-3 mt-6 font-medium tracking-widest text-white uppercase bg-blue-600 shadow-lg rounded focus:outline-none hover:bg-blue-700 hover:shadow-none"
+          disabled={loading}
         >
-          Sign up
+          {loading ? 'Loading...' : 'Sign up'}
         </button>
         <p className="flex justify-between mt-4 text-xs text-gray-500 cursor-pointer hover:text-black">
           Already registered?
