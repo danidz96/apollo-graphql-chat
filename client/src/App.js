d0cdeb08ae3d30eb9760e24e8fb733a126e93c56
Dani Domínguez <dani.d.zamorano@gmail.com>
2021-01-30 15:26:37 +0100

    create register page

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,11 @@
+import Register from './Pages/Register';
+
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World!</h1>
+    <div className="grid grid-cols-1 items-center min-h-screen bg-blue-50 md:grid-cols-8">
+      <div className="col-auto md:col-span-4 md:col-start-3">
+        <Register />
+      </div>
     </div>
   );
 }
