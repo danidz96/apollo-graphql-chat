2cdc336d3127465177bd788f3971e18b578cf8e4
Dani Domínguez <dani.d.zamorano@gmail.com>
2021-02-04 20:05:59 +0100

    add react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,23 @@
 import Register from '@pages/Register';
 import ApolloProvider from './ApolloProvider';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Login from './pages/Login';
+import Home from './pages/Home';
 
 function App() {
   return (
     <ApolloProvider>
-      <div className="grid grid-cols-1 items-center min-h-screen bg-blue-50 md:grid-cols-8">
-        <div className="col-auto md:col-span-4 md:col-start-3">
-          <Register />
-        </div>
-      </div>
+      <BrowserRouter>
+        <Switch>
+          <div className="grid grid-cols-1 items-center min-h-screen bg-blue-50 md:grid-cols-8">
+            <div className="col-auto md:col-span-4 md:col-start-3">
+              <Route path="/" exact component={Home} />
+              <Route path="/register" component={Register} />
+              <Route path="/login" component={Login} />
+            </div>
+          </div>
+        </Switch>
+      </BrowserRouter>
     </ApolloProvider>
   );
 }
