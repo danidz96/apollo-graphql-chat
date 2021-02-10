4285e741d3bcccea4d308ea9fe183dec445c74a3
Dani Domínguez <dani.d.zamorano@gmail.com>
2021-02-10 19:32:05 +0100

    create a navbar

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Login from '@pages/Login';
 import Home from '@pages/Home';
 import { AuthProvider } from '@context/auth';
+import Navbar from '@components/Navbar';
 
 function App() {
   return (
@@ -11,11 +12,14 @@ function App() {
       <AuthProvider>
         <BrowserRouter>
           <Switch>
-            <div className="grid grid-cols-1 items-center min-h-screen bg-blue-50 md:grid-cols-8">
-              <div className="col-auto md:col-span-4 md:col-start-3">
-                <Route path="/" exact component={Home} />
-                <Route path="/register" component={Register} />
-                <Route path="/login" component={Login} />
+            <div>
+              <Navbar />
+              <div className="grid grid-cols-1 items-center min-h-screen bg-blue-50 md:grid-cols-8">
+                <div className="col-auto md:col-span-4 md:col-start-3">
+                  <Route path="/" exact component={Home} />
+                  <Route path="/register" component={Register} />
+                  <Route path="/login" component={Login} />
+                </div>
               </div>
             </div>
           </Switch>
---
+++ client/src/components/Navbar.js
@@ -0,0 +1,32 @@
+import { Link } from 'react-router-dom';
+import { useAuthState, useAuthDispatch } from '@context/auth';
+
+function Navbar() {
+  const { user } = useAuthState();
+  const dispatch = useAuthDispatch();
+
+  const logout = () => {
+    dispatch({ type: 'LOGOUT' });
+  };
+
+  return (
+    <div className="h-10 w-9/12 flex justify-end items-center">
+      {user ? (
+        <Link to="/login">
+          <button onClick={logout}>Logout</button>
+        </Link>
+      ) : (
+        <>
+          <Link to="/login">
+            <button>Login</button>
+          </Link>
+          <Link to="/register" className="ml-3">
+            <button>Register</button>
+          </Link>
+        </>
+      )}
+    </div>
+  );
+}
+
+export default Navbar;
