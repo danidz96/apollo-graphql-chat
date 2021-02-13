61321f9155dcf11a28838f9bca5042f443ee81d0
Dani Domínguez <dani.d.zamorano@gmail.com>
2021-02-13 13:51:28 +0100

    get latestMessage on getUsers

--- graphql/typeDefs.js
+++ graphql/typeDefs.js
@@ -3,9 +3,10 @@ const { gql } = require('apollo-server');
 module.exports = gql`
   type User {
     username: String!
-    email: String!
     createdAt: String!
     token: String
+    imageUrl: String
+    latestMessage: Message
   }
   type Query {
     getUsers: [User]!
