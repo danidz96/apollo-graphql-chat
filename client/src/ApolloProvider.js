0c7d5127ce5f4b69576021df9fa97fc84cffc4ef
Dani Domínguez <dani.d.zamorano@gmail.com>
2021-02-13 13:25:13 +0100

    setup authorization with apollo

--- client/src/ApolloProvider.js
+++ client/src/ApolloProvider.js
@@ -1,7 +1,24 @@
-import { ApolloClient, InMemoryCache, ApolloProvider as Provider } from '@apollo/client';
+import React from 'react';
+import { ApolloClient, InMemoryCache, ApolloProvider as Provider, createHttpLink } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
+const httpLink = createHttpLink({
+  uri: 'http://localhost:4000',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('token');
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token.replace(/['"]+/g, '')}` : '',
+    },
+  };
+});
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000',
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
