3a60471981cdf1354426d03ea938cba1c44cc2bf
Dani Domínguez <dani.d.zamorano@gmail.com>
2021-03-01 20:45:31 +0100

    split messages sectio into single component

--- client/src/Pages/home/Home.js
+++ client/src/Pages/home/Home.js
@@ -1,61 +1,14 @@
-import { useState, useEffect } from 'react';
-import { gql, useQuery, useLazyQuery } from '@apollo/client';
-import UserChatRow from '@components/UserChatRow';
+import { useState } from 'react';
 import Users from './Users';
-
-const GET_USERS = gql`
-  query getUsers {
-    getUsers {
-      username
-      createdAt
-      imageUrl
-      latestMessage {
-        uuid
-        from
-        to
-        content
-        createdAt
-      }
-    }
-  }
-`;
-
-const GET_MESSAGES = gql`
-  query getMessages($from: String!) {
-    getMessages(from: $from) {
-      uuid
-      from
-      to
-      content
-      createdAt
-    }
-  }
-`;
+import Messages from './Messages';
 
 function Home() {
-  const { loading, data, error } = useQuery(GET_USERS);
   const [selectedUser, setSelectedUser] = useState(null);
-
-  const [getMessages, { loading: messagesLoading, data: messagesData }] = useLazyQuery(GET_MESSAGES);
-
-  useEffect(() => {
-    if (selectedUser) {
-      getMessages({ variables: { from: selectedUser } });
-    }
-  }, [getMessages, selectedUser]);
-
-  if (messagesData) console.log(messagesData.getMessages);
 
   return (
     <div className="w-full h-96 bg-white grid grid-cols-6 rounded-sm shadow-md" style={{ height: '85vh' }}>
       <Users setSelectedUser={setSelectedUser} />
-      <div className="col-span-4">
-        {messagesData?.getMessages.length > 0 ? (
-          messagesData.getMessages.map((message) => <p key={message.uuid}>{message.content}</p>)
-        ) : (
-          <p>You are now connected.</p>
-        )}
-      </div>
+      <Messages selectedUser={selectedUser} />
     </div>
   );
 }
