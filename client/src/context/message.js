b84ccf052005fca49874e00c05ce093ca6993078
Dani Domínguez <dani.d.zamorano@gmail.com>
2021-03-15 21:39:41 +0100

    save selected user messages in context

--- client/src/context/message.js
+++ client/src/context/message.js
@@ -4,15 +4,25 @@ const MessageStateContext = createContext();
 const MessageDispatchContext = createContext();
 
 const messageReducer = (state, action) => {
+  let users;
   switch (action.type) {
     case 'SET_USERS':
       return {
         ...state,
         users: action.payload,
       };
+    case 'SET_USER_MESSAGES':
+      const { username, messages } = action.payload;
+      users = [...state.users];
+      const userIndex = users.findIndex((user) => user.username === username);
+      users[userIndex] = { ...users[userIndex], messages };
 
+      return {
+        ...state,
+        users,
+      };
     case 'SET_SELECTED_USER':
-      const users = state.users.map((user) => ({ ...user, selected: user.username === action.payload }));
+      users = state.users.map((user) => ({ ...user, selected: user.username === action.payload }));
       return { ...state, users };
     default:
       throw new Error(`Unknown action type: ${action.type}`);
